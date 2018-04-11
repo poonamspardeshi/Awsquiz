3f811a9485f3dbdbac3a0f23ec7c1d27a0ad3968
poonamspardeshi <poonam17293@gmail.com>
2018-04-11 16:50:33 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, json, url_for, redirect
+from flaskext.mysql import MySQL
+import plotly
+#plotly.tools.set_credentials_file(username='ganeshcodes', api_key='9QIJefjnbz6Y0arQD7ww')
+#import plotly.plotly as py
+from plotly.offline import plot
+import plotly.graph_objs as go
+#py.sign_in('ganeshcodes', '9QIJefjnbz6Y0arQD7ww')
+
+#kmeans imports
+import pandas as pd
+from scipy import stats
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pylab as pl
+
+app = Flask(__name__, static_url_path='')
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'sampledb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route('/hello/<user>')
+def hello_name(user):
+    return render_template('hello.html', name = user)
+
+
+@app.route('/listcoursesform')
+def listcoursesform():
+    return render_template('listcourses.html', data=[])
+
+@app.route('/listcourses', methods=['POST'])
+def listcourses():
+    # Get inputs from form
+    room =  request.form['room']
+    q = "select distinct Course from classes where Room like '%"+room+"%'"
+    print(q)
+    # execute and get results
+    cursor = mysql.connect().cursor()
+    cursor.execute(q)
+    results = cursor.fetchall()
+    resp = []
+    for i in range(len(results)):
+        resp.append(results[i][0])
+    print(resp)
+    return render_template('listcourses.html', data=resp)
